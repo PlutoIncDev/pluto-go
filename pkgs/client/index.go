837727e6d129b0352095e9f88b5f8648a083ba38
Calum Webb <calumpeterwebb@icloud.com>
2021-06-07 14:12:05 -0700

    simplified providers and added graceful shutdown for lifecycles

---
+++ pkgs/client/index.go
@@ -0,0 +1,102 @@
+package client
+
+import (
+	"context"
+	"fmt"
+	"log"
+	"pluto/pkgs/providers/base"
+	"sync"
+	"time"
+)
+
+type Client struct {
+	// config
+	name                    string
+	maxShutdownDuration     time.Duration // max duration for shutting down all the providers when the server closes before force quitting
+	isFinishedSleepDuration time.Duration // how often to poll the services when shutting down to check if they're ready to stop
+
+	// internal variables
+	providers     []base.Provider
+	cancelCtxFunc context.CancelFunc
+}
+
+func New(name string) *Client {
+	return &Client{
+		name:                    name,
+		maxShutdownDuration:     time.Second * 30,
+		isFinishedSleepDuration: time.Second,
+	}
+}
+
+func (c *Client) Start(blocking bool) {
+	log.Println(fmt.Sprintf("Starting the %s service", c.name))
+
+	ctx, cancel := context.WithCancel(context.Background())
+	c.cancelCtxFunc = cancel // assign for later use
+	var wg sync.WaitGroup
+
+	for _, p := range c.providers {
+		wg.Add(1)
+
+		provider := p
+		go func() {
+			provider.Setup()
+
+			go provider.Run(ctx)
+
+			for {
+				select {
+				case <-ctx.Done():
+					provider.Shutdown()
+					return
+				}
+			}
+		}()
+	}
+
+	if blocking {
+		wg.Wait()
+	}
+}
+
+func (c *Client) Stop() {
+	log.Println("Attempting to stop service")
+	c.cancelCtxFunc()
+
+	log.Println("Issued cancellation signal to providers")
+
+	startShutdownTime := time.Now()
+
+	// check if all providers are finished
+	for {
+		if time.Now().Sub(startShutdownTime) > c.maxShutdownDuration {
+			log.Println("Some providers took longer to shutdown than the maxShutdownDuration")
+			break
+		}
+
+		// check if providers are ready to shut down every second
+		secsRemaining := int((c.maxShutdownDuration - time.Now().Sub(startShutdownTime)).Seconds())
+
+		log.Println(fmt.Sprintf("Waiting for providers to shutdown (%ds remaining before force quitting)", secsRemaining))
+		time.Sleep(c.isFinishedSleepDuration)
+
+		providerStillRunning := false
+		for _, p := range c.providers {
+			if !p.IsFinished() {
+				providerStillRunning = true
+			}
+		}
+
+		if !providerStillRunning {
+			// if we reached here, all the providers are finished
+			break
+		}
+	}
+
+	log.Println("Shutdown service complete")
+}
+
+func (c *Client) RegisterProvider(p base.Provider) {
+	// todo: stop providers being registered twice reflect type/ name maybe?
+	c.providers = append(c.providers, p)
+}
--- pkgs/providers/http/index.go
+++ pkgs/providers/http/index.go
@@ -1,65 +1,16 @@
 package http
 
-import (
-	"fmt"
-	"github.com/gin-gonic/gin"
-	"log"
-)
-
 func NewProvider(port string) *Provider {
 	return &Provider{
+		// config
 		port: port,
+
+		// internal
+		finished: false,
 	}
 }
 
-func (p *Provider) Setup() {
-	// TODO: Setup the HTTP server
-	log.Println(fmt.Sprintf("Starting HTTP Server on port=%s", p.port))
-
-	gin.SetMode(gin.ReleaseMode)
-	engine := gin.New()
-	p.server = engine
-
-	// todo: middleware
-	// todo: logger middleware
-	// todo: limit https://github.com/aviddiviner/gin-limit (add default? 1,000?)
-
-	// register all the endpoints with the server
-	for _, e := range p.endpoints {
-		// TODO: test that this loop variable capture issue is fixed!
-		endpoint := e // loop variable capture issue if we dont copy e into endpoint
-
-		log.Println(fmt.Sprintf("Registering [%s] %s to HTTP Provider", endpoint.method, endpoint.path))
-		p.server.Handle(string(endpoint.method), endpoint.path, func(ctx *gin.Context) {
-			endpoint.handler(NewContext(ctx))
-		})
-	}
-}
-
-func (p *Provider) Run() {
-	// todo: Run HTTP Server
-	_ = p.server.Run(fmt.Sprintf(":%s", p.port))
-}
-
-func (p *Provider) Shutdown() {
-	// TODO: Shutdown the HTTP Server
-}
-
-func (p *Provider) RegisterEndpoint(method Method, path string, handler endpointHandler) {
-	// check if the endpoint is already registered
-	for _, e := range p.endpoints {
-		if e.path == path && e.method == method {
-			panic(fmt.Sprintf("Endpoint with method %s and path %s should only be registered once", method, path))
-		}
-	}
-
+func (p *Provider) RegisterEndpoint(method string, path string, handler endpointHandler) {
 	e := NewEndpoint(method, path, handler)
 	p.endpoints = append(p.endpoints, e)
-	log.Println(p.endpoints)
 }
-
-func (p *Provider) GetEndpoints() []*endpoint {
-	return p.endpoints
-}
-
-// TODO: add middleware?
